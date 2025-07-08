7bea5cdff3c172c2269359252bef722287ab94cb
Rhoncuss Dignissim <157534895+Rhoncuss@users.noreply.github.com>
2025-07-08 08:17:07 -0700

    Add files via upload

---
+++ assets/external-link-DjkcAWWp.js
@@ -0,0 +1,8 @@
+import{b as l}from"./core-Su_g1Q0x.js";import"./index-CJxGCnDK.js";import"./index.es-BO8g8zjT.js";import"./events-DQ172AOg.js";const t=l`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M3.74 3.99a1 1 0 0 1 1-1H11a1 1 0 0 1 1 1v6.26a1 1 0 0 1-2 0V6.4l-6.3 6.3a1 1 0 0 1-1.4-1.42l6.29-6.3H4.74a1 1 0 0 1-1-1Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{t as externalLinkSvg};
