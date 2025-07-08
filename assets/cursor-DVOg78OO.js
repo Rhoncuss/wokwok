bf08baf5e6d52508cd6f84e1fab54834c65ab2bf
Rhoncuss <webdestra@gmail.com>
2025-07-08 10:20:59 -0700

    solokotok

---
+++ assets/cursor-DVOg78OO.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-BWSa6l5C.js";import"./index-DbspBTzI.js";import"./index.es-WKkZfNhi.js";import"./events-DQ172AOg.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
